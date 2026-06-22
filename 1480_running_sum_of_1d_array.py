3d4b07a76eddadeb6f869d8ba5847c90e283974b
Abiraminayagi S <abiramisgp@gmail.com>
2026-06-22 20:32:04 +0530

    Updation

--- 1480_running_sum_of_1d_array.py
+++ 1480_running_sum_of_1d_array.py
@@ -28,6 +28,6 @@ def running_sum(nums):
 
 
 if __name__ == "__main__":
-    print(running_sum([1, 2, 3, 4]))        # [1,3,6,10]
-    print(running_sum([1, 1, 1, 1, 1]))     # [1,2,3,4,5]
-    print(running_sum([3, 1, 2, 10, 1]))    # [3,4,6,16,17]
+    print(running_sum([1, 2, 3, 4]))        
+    print(running_sum([1, 1, 1, 1, 1]))
+    print(running_sum([3, 1, 2, 10, 1]))   
